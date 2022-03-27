0d521e28896580bbe39c6a6250db9a4c68c94cf9
AudriusAnusauskas <82049110+AudriusAnusauskas@users.noreply.github.com>
2022-03-27 12:06:12 +0300

    about component complete

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -9,6 +9,7 @@ function intro() {
                <div className="i-left-wrapper">
                    <h2 className="i-intro">Hello, my name is</h2>
                    <h1 className="i-name">Audrius Anusauskas</h1>
+                   <h2 className="i-intro">I am a</h2>
                    <div className="i-title">
                        <div className="i-title-wrapper">
                            <div className="i-title-item">Front End Developer</div>
