df443db5078f0a5d15f8224a4e9aa6de2ba39722
AudriusAnusauskas <82049110+AudriusAnusauskas@users.noreply.github.com>
2022-03-27 15:39:07 +0300

    ProductList and Product components

---
+++ src/data.js
@@ -0,0 +1,45 @@
+import p1 from './img/projects/u7m.png'
+import p2 from './img/projects/record.png'
+import p3 from './img/projects/coyote.png'
+import p4 from './img/projects/recipe.png'
+import p5 from './img/projects/p5.webp'
+import p6 from './img/projects/p6.webp'
+import p7 from './img/projects/p1.png'
+
+export const products = [
+    {
+      id: 1,
+      img: p1,
+      link: "https://uz7mariu.lt/",
+    },
+    {
+      id: 2,
+      img: p2,
+      link: "http://htmlpreview.github.io/?https://github.com/AudriusAnusauskas/portfolio/blob/main/shopify-project/record.html",
+    },
+    {
+      id: 3,
+      img: p3,
+      link: "http://htmlpreview.github.io/?https://github.com/AudriusAnusauskas/portfolio/blob/main/shopify-project-2/index.html",
+    },
+    {
+      id: 4,
+      img: p4,
+      link: "https://github.com/AudriusAnusauskas/recipebook",
+    },
+    {
+      id: 5,
+      img: p5,
+      link: "http://lama.dev",
+    },
+    {
+      id: 6,
+      img: p6,
+      link: "http://lama.dev",
+    },
+    {
+        id: 7,
+        img: p7,
+        link: "http://lama.dev",
+      }
+  ];
