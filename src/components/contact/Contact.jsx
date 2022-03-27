7f1e900d29e300f48fbb70a9336f77be085ec618
AudriusAnusauskas <82049110+AudriusAnusauskas@users.noreply.github.com>
2022-03-27 22:38:17 +0300

    final project

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,12 +2,16 @@ import './contact.css'
 import Phone from '../../img/phone.png'
 import Email from '../../img/email.png'
 import Address from '../../img/address.png'
-import { useRef, useState } from 'react'
+import { useContext, useRef, useState } from 'react'
 import emailjs from '@emailjs/browser';
+import { ThemeContext } from '../../context'
 
 export const Contact = () => {
     const formRef = useRef()
     const [done, setDone] = useState(false)
+
+    const theme = useContext(ThemeContext)
+  const darkMode = theme.state.darkMode
 
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -65,10 +69,10 @@ export const Contact = () => {
                         freelancing if the right project comes along.
                     </p>
                     <form ref={formRef} onSubmit={handleSubmit}>
-                        <input type="text" placeholder="Name" name="user_name" />
-                        <input type="text" placeholder="Subject" name="user_subject" />
-                        <input type="text" placeholder="Email" name="user_email" />
-                        <textarea rows="5" placeholder="Message" name="message" ></textarea>
+                        <input style={{backgroundColor: darkMode && '#333'}} type="text" placeholder="Name" name="user_name" />
+                        <input style={{backgroundColor: darkMode && '#333'}} type="text" placeholder="Subject" name="user_subject" />
+                        <input style={{backgroundColor: darkMode && '#333'}} type="text" placeholder="Email" name="user_email" />
+                        <textarea style={{backgroundColor: darkMode && '#333'}} rows="5" placeholder="Message" name="message" ></textarea>
                         <button>Submit</button>
                         <div className="c-thankyou">
                             {done && ' Thank you!'}
