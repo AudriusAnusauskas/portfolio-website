01dd228706d91c1439d3c0f831e3853b75bd8201
Audrius Ausauskas <aaaudrius@gmail.com>
2023-01-20 15:54:06 +0200

    update responsiveness

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,85 +1,96 @@
-import './contact.css'
-import Phone from '../../img/phone.png'
-import Email from '../../img/email.png'
-import Address from '../../img/address.png'
-import { useContext, useRef, useState } from 'react'
-import emailjs from '@emailjs/browser';
-import { ThemeContext } from '../../context'
+import "./contact.css";
+import Phone from "../../img/phone.png";
+import Email from "../../img/email.png";
+import Address from "../../img/address.png";
+import { useContext, useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import { ThemeContext } from "../../context";
 
 export const Contact = () => {
-    const formRef = useRef()
-    const [done, setDone] = useState(false)
+  const formRef = useRef();
+  const [done, setDone] = useState(false);
 
-    const theme = useContext(ThemeContext)
-  const darkMode = theme.state.darkMode
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        emailjs.sendForm(
-            'service_z946d79', 
-            'template_fs551dc', 
-            formRef.current, 
-            'szFDPNBj-MT8OX1XK')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        "service_z946d79",
+        "template_fs551dc",
+        formRef.current,
+        "szFDPNBj-MT8OX1XK"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
-          setDone(true)
+          setDone(true);
           e.target.reset();
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
-    }
+        }
+      );
+  };
 
-    return (
-        <div className="c">
-            <div className="c-bg"></div>
-            <div className="c-wrapper">
-                <div className="c-left">
-                    <h2 className="c-title">Let's talk about your new project</h2>
-                    <div className="c-info">
-                        <div className="c-info-item">
-                            <img 
-                            src= {Phone} 
-                            alt=""
-                            className="c-icon"
-                            />
-                            +370(616)08758
-                        </div>
-                        <div className="c-info-item">
-                            <img 
-                            src= {Email} 
-                            alt=""
-                            className="c-icon"
-                            />
-                            anusauskas.audrius@gmail.com
-                        </div>
-                        <div className="c-info-item">
-                            <img 
-                            src= {Address} 
-                            alt=""
-                            className="c-icon"
-                            />
-                            Aušrinės 21, Vilnius, LT08402, Lithuania
-                        </div>
-                    </div>
-                </div>
-                <div className="c-right">
-                    <p className="c-desc">
-                        <b>What’s your story?</b> Get in touch. Always available for
-                        freelancing if the right project comes along.
-                    </p>
-                    <form ref={formRef} onSubmit={handleSubmit}>
-                        <input style={{backgroundColor: darkMode && '#333'}} type="text" placeholder="Name" name="user_name" />
-                        <input style={{backgroundColor: darkMode && '#333'}} type="text" placeholder="Subject" name="user_subject" />
-                        <input style={{backgroundColor: darkMode && '#333'}} type="text" placeholder="Email" name="user_email" />
-                        <textarea style={{backgroundColor: darkMode && '#333'}} rows="5" placeholder="Message" name="message" ></textarea>
-                        <button>Submit</button>
-                        <div className="c-thankyou">
-                            {done && ' Thank you!'}
-                        </div>
-                    </form>
-                </div>
+  return (
+    <div className="c">
+      <div className="c-bg"></div>
+      <div className="c-wrapper">
+        <div className="c-left">
+          <h2 className="c-title">Let's talk about your new project</h2>
+          <div className="c-info">
+            <div className="c-info-item">
+              <img src={Phone} alt="" className="c-icon" />
+              +370(616)08758
             </div>
+            <div className="c-info-item">
+              <img src={Email} alt="" className="c-icon" />
+              anusauskas.audrius@gmail.com
+            </div>
+            <div className="c-info-item">
+              <img src={Address} alt="" className="c-icon" />
+              Aušrinės 21, Vilnius, LT08402, Lithuania
+            </div>
+          </div>
         </div>
-    )
-}
+        <div className="c-right">
+          <p className="c-desc">
+            <b>What’s your story?</b> Get in touch. Always available for
+            freelancing if the right project comes along.
+          </p>
+          <form ref={formRef} onSubmit={handleSubmit}>
+            <input
+              style={{ backgroundColor: darkMode && "#333" }}
+              type="text"
+              placeholder="Name"
+              name="user_name"
+            />
+            <input
+              style={{ backgroundColor: darkMode && "#333" }}
+              type="text"
+              placeholder="Subject"
+              name="user_subject"
+            />
+            <input
+              style={{ backgroundColor: darkMode && "#333" }}
+              type="text"
+              placeholder="Email"
+              name="user_email"
+            />
+            <textarea
+              style={{ backgroundColor: darkMode && "#333" }}
+              rows="5"
+              placeholder="Message"
+              name="message"
+            ></textarea>
+            <button>Submit</button>
+            <div className="c-thankyou">{done && " Thank you!"}</div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
